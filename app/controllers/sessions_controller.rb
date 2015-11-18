ded37a5a4178b19195b0443e386c1eb9e032ad95
Sliktikakis <elie.oriol@orange.fr>
2015-11-18 17:13:46 +0100

    Logging fini

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(identifiant: params[:session][:identifiant])
-  	if user && user.authenticate(params[:session][:password])
-  	  log_in user
-  	  redirect_to user
+  	@user = User.find_by(identifiant: params[:session][:identifiant])
+  	if @user && @user.authenticate(params[:session][:password])
+  	  log_in @user
+  	  redirect_to @user
   	else
   	  flash.now[:danger] = "Combinaison identifiant/mot de passe invalide"
   	  render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,20 @@
 class User < ActiveRecord::Base
-	validates :nom, 		presence: true,
+  validates :nom, 			presence: true,
+  							length: {maximum: 25}
+  validates :prenom, 		presence: true,
 							length: {maximum: 25}
-	validates :prenom, 		presence: true,
-							length: {maximum: 25}
-	validates :identifiant, presence: true,
+  validates :identifiant, 	presence: true,
 							length: {maximum: 25, minimum: 4},
 							uniqueness: {case_sensitive: false}
-	has_secure_password
-	validates :password,	presence: true,
+  has_secure_password
+  validates :password,		presence: true,
 							length: {minimum: 6}
-
+  class << self						
 	# Returns the hash digest of the given string.
-    def User.digest(string)
+    def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
     end
+  end
 end
