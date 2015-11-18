493fdc4bbf55bc0d5c7ce8ddad6d3c4da9bc89b2
Sliktikakis <elie.oriol@orange.fr>
2015-11-18 17:03:52 +0100

    Logging fini

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  	  log_in @user
   	  flash[:success] = "Vous pouvez désormais accéder à l'application PQI !"
   	  redirect_to @user
   	else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,6 +12,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do
@@ -24,5 +26,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                               				password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
