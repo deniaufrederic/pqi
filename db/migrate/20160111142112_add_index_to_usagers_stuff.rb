8fd5b740db94636cba35be043c0d1c5190b67991
Sliktikakis <elie.oriol@orange.fr>
2016-01-11 15:23:23 +0100

    Push heroku

--- db/migrate/20160111142112_add_index_to_usagers_stuff.rb
+++ db/migrate/20160111142112_add_index_to_usagers_stuff.rb
@@ -1,7 +1,6 @@
 class AddIndexToUsagersStuff < ActiveRecord::Migration
   def change
   	add_index :usagers, :ville
-  	add_index :usagers, :signale
-  	add_index :usagers, :sexe
+  	add_index :usagers, :nom
   end
 end
