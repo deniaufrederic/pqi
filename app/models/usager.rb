51e65f56803c1baf082cc533a424be1cbe736ebd
Sliktikakis <elie.oriol@orange.fr>
2016-01-14 16:34:39 +0100

    Bug date de naissance

--- app/models/usager.rb
+++ app/models/usager.rb
@@ -1,6 +1,7 @@
 class Usager < ActiveRecord::Base
   before_save { nom.upcase! }
   before_save { self.prenom = prenom.split(' ').map(&:capitalize).join(' ').split('-').map(&:capitalize).join('-') }
+  before_save { self.date_naissance = date_naissance.strftime("%d/%m/%y") }
   default_scope -> { order(pqi: :desc, nom: :asc) }
   validate :at_least_one
   VALID_TEL_REGEX = /\A\d{10}\Z/
