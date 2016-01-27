dc80bcbc4423870756ee17d1bc3088173c2b2a65
Sliktikakis <elie.oriol@orange.fr>
2016-01-27 14:09:26 +0100

    Nouveau système de rencontre

--- db/migrate/20160107123705_add_signalement_to_usagers.rb
+++
@@ -1,5 +0,0 @@
-class AddSignalementToUsagers < ActiveRecord::Migration
-  def change
-    add_column :usagers, :signalement, :string
-  end
-end
--- db/migrate/20160113111917_add_details_to_usagers.rb
+++
@@ -1,5 +0,0 @@
-class AddDetailsToUsagers < ActiveRecord::Migration
-  def change
-    add_column :usagers, :details, :text
-  end
-end
