868d6421b2cab3fe996ac4ba83b64ef2173c14b7
Sliktikakis <elie.oriol@orange.fr>
2016-03-02 14:21:43 +0100

    Stats /maraude

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -13,14 +13,14 @@ module StatsHelper
     elsif is_number?(lollipop[n-3].split('-').last)
       session[:stored_dates] = [lollipop[n-4], lollipop[n-3]]
       if lollipop[n-2] == "type"
-        session[:stored_type] = lollipop.last.split('%20').join(' ').split('%C3%A9').join('é')
+        session[:stored_type] = lollipop.last.split('%20').join(' ').split('%C3%A9').join('é').split('%C3%B4').join('ô')
         session[:stored_ville] = nil
       elsif lollipop[n-2] == "ville"
-        session[:stored_ville] = lollipop.last.split('%20').join(' ').split('%C3%A9').join('é')
+        session[:stored_ville] = lollipop.last.split('%20').join(' ').split('%C3%A9').join('é').split('%C3%B4').join('ô')
         session[:stored_type] = nil
       else
-        session[:stored_type] = lollipop[n-2].split('%20').join(' ').split('%C3%A9').join('é')
-        session[:stored_ville] = lollipop.last.split('%20').join(' ').split('%C3%A9').join('é')
+        session[:stored_type] = lollipop[n-2].split('%20').join(' ').split('%C3%A9').join('é').split('%C3%B4').join('ô')
+        session[:stored_ville] = lollipop.last.split('%20').join(' ').split('%C3%A9').join('é').split('%C3%B4').join('ô')
       end
     else
       session[:stored_dates] = nil
